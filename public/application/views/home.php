eb72c3c58e5b2d0aaa82dec7272784cf9e26d7ad
CatarinaAz <Azevedocatarina53@gmail.com>
2021-03-12 02:16:52 +0000

    em andamento

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -20,7 +20,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<div class="col-12">
 			<div class="jumbotron">
   				<h1 class="display-4">Bem vindo!</h1>
-  				<p class="lead">Apredendo a criar site</p>
+  				<p class="lead">Pet lovers, o site para quem ama seu animalzinho♥</p>
 				</div>
 			</div>
 		</div>
@@ -44,57 +44,67 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 		</div>
 		<div class="row cont">
 			<div class="col-6">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<h3>Shih-tzu</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/jiu_shitzu.jpg" alt="" class = "img-fluid">
+				<p>
+					Focinho achatado, olhos amendoados um pouco saltados e personalidade marcante: este é o Shih Tzu! 
+					Conhecidos por serem muito companheiros, os Shih Tzus não são cães de caça ou esportistas, o propósito 
+					de suas vidas é amar e ser amado pelos seus pais humanos, servindo como um incrível cão de companhia.
+				</p>
+			</div>
+			<div class="col-6">
+				<h3>Chihuahua</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/chibalba.jpg" alt="" class = "img-fluid">
+				<p>
+					Considerado um dos menores cães do mundo, o chihuahua só não tem tamanho: 
+					seu jeito atrevido e brincalhão faz com que os pais humanos desta raça sejam 
+					realmente apaixonados por seus pequenos filhotes. Se engana quem acha que, 
+					por seu pequeno porte, ele é do tipo medroso. Ao contrário disso, o cachorro 
+					chihuahua tende a ser destemido e cheio de coragem.
+				</p>
+			</div>
+			<div class="col-6">
+				<h3>Pug</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pugo.jpg" alt="" class = "img-fluid">
 				<p>
 					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
 					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
 				</p>
 			</div>
 			<div class="col-6">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<h3>Lulu da Pomerania</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/lula_da_pomerania.jpg" alt="" class = "img-fluid">
 				<p>
-					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
-					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+					Se trata de uma raça de cachorro de companhia muito dócil e brincalhona, 
+					que além de se dar muito bem com seus pais humanos. É muito afetuoso e 
+					não é considerado um cão agressivo, mas pode ser receoso com desconhecidos num 
+					primeiro momento. 
 				</p>
 			</div>
-			<div class="col-3">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+			<div class="col-6">
+				<h3>Yorkshire Terrier</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/nhocxaire.jpg" alt="" class = "img-fluid">
 				<p>
-					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
-					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+					Acredita-se que a raça tenha sido criada por camponeses para auxiliar na caça de roedores 
+					(já que o seu tamanho permitia fácil acesso às tocas). Através do cruzamento de diversos 
+					outros Terriers e também do Maltês, surgiram os Yorkshire Terriers. Hoje em dia o Yorkshire 
+					é um cãozinho muito popular no mundo todo.
 				</p>
 			</div>
-			<div class="col-3">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+			<div class="col-6">
+				<h3>Poodle</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/poodle.jpg" alt="" class = "img-fluid">
 				<p>
-					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
-					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
-				</p>
-			</div>
-			<div class="col-3">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
-				<p>
-					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
-					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
-				</p>
-			</div>
-			<div class="col-3">
-				<h3>Título</h3>
-				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
-				<p>
-					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
-					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+					Uma das raças mais conhecidas no mundo todo, o Poodle possui diversas características 
+					que faz com sejam amados por todos. São ótimos nadadores e amam brincar na água. Extre-
+					mamente apegados ao dono e inteligentes, possuem facilidade em se relacionar com crianças 
+					e outros animais.
 				</p>
 			</div>
 		</div>
 		<div class="row">
 			<div class="col-12 rodape">
-				<p>É assim que faz rodapé</p>
+				<p>Css não está funcionando</p>
 			</div>
 		</div>
 	</div>
