248ca62a8f8d7e8d58b968a41a22b9d72b712d7c
CatarinaAz <Azevedocatarina53@gmail.com>
2021-03-12 00:08:08 +0000

    atualizando

--- public/application/views/home.php
+++ public/application/views/home.php
@@ -8,8 +8,96 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Home - Site PW</title>
+	<link rel= "stylesheet" href="<?php echo base_url() ?>assets/reset.css">
+    <link rel= "stylesheet" href="<?php echo base_url() ?>assets/bootstrap/css/bootstrap.min.css">
+    <link rel= "stylesheet" href="<?php echo base_url() ?>assets/css/principal.css">
 </head>
 <body>
-	<h1>Seja bem vindo!</h1>
+	
+	
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+			<div class="jumbotron">
+  				<h1 class="display-4">Salve Quebrada!</h1>
+  				<p class="lead">Apredendo a criar site bro</p>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12">
+			<ul class="nav">
+				<li class="nav-item">
+					<a class="nav-link active" href="#">Active</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#">Link</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+				</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row cont">
+			<div class="col-6">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+			<div class="col-6">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+			<div class="col-3">
+				<h3>Título</h3>
+				<img src = "<?php echo base_url() ?>/assets/imagens/pug-personalidade-pet.jpg" alt="" class = "img-fluid">
+				<p>
+					Pug é uma raça de cão de companhia de pequeno porte, com focinho achatado e cauda enrolada. São cães braquicefálicos, ou seja, têm o focinho "achatado". 
+					Cães com essa características tem o sistema respiratório superior comprimido e portanto não toleram muito exercício físico.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="col-12 rodape">
+				<p>É assim que faz rodapé</p>
+			</div>
+		</div>
+	</div>
+
 </body>
 </html>
